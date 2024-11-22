7ace7672628cdb6a7383ab542762c0524610ea27
Leonardo Mazzuca <leonardodesouzamazzuca8@gmail.com>
2024-11-22 13:43:01 -0300

    uploading files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //@ts-ignore
+  experimental: {serverActions: true},
 };
 
 export default nextConfig;
